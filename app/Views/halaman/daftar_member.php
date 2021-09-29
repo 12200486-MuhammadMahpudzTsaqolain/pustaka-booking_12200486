a454e1eb575f776f91fb0e2df9b35fb9ca1832c1
12200486-MuhammadMahpudzTsaqolain <deutmahfudz@gmail.com>
2021-09-29 22:23:19 +0700

    tugas pertemuan 2

---
+++ app/Views/halaman/daftar_member.php
@@ -0,0 +1,9 @@
+<html>
+    <title>
+        Daftar Member
+    </title>
+    <body>
+        hai... kamu mau menjadi member?
+        <p>Isikan data diri kamu dulu ya agar kami kenal dengan member kami</p>
+    </body>
+</html>
---
+++ app/Views/halaman/login.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Perpustakaan login</title>
+    </head>
+    <body>
+        <h1>Selamat datang di Sistem Informasi Perpustakaan</h1>
+        <p>
+            Ini adalah layanan perpustakaan digital masa kini
+            <a href="<?=base_url(relativePath: '/daftar-member')?>">klik daftar</a> untuk mendaftar menjadi member
+        </p>
+    </body>
+</html>
