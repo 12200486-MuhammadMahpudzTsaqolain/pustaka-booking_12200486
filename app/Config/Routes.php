fcea8d96173543014fc0563b032b793a1942921f
12200486-MuhammadMahpudzTsaqolain <deutmahfudz@gmail.com>
2021-10-20 22:50:23 +0700

    pertemuan 5

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,10 +32,16 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
-$routes->get('/hello', 'SelamatDatang::hal_awal');
-$routes->get('/login', 'SelamatDatang::beranda_login');
-$routes->post('login','Login::cekLogin');
-$routes->get('/daftar-member', 'SelamatDatang::daftar_member');
+$routes->get( '/hello',  'SelamatDatang::hal_awal');
+$routes->post('/login', 'Login::cekLogin');
+$routes->get( '/daftar-member', 'SelamatDatang::daftar_member');
+$routes->get('/beranda', 'SelamatDatang::hal_beranda', ['filter'=>'auth']);
+
+$routes->get( '/login',  'SelamatDatang::beranda_login', ['filter'=>'autoin']);
+$routes->get('/logout', function(){
+    Services::session()->destroy();
+    return redirect()->to('/login'); 
+});
 
 /*
  * --------------------------------------------------------------------
